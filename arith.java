10ea82147d89fead9e93128e4132467298d52e6d
Sachin Maurya <kumarsachin67671@gmail.com>
2024-04-29 23:20:16 +0530

    PACKAGE  IN JAVA

---
+++ arith.java
@@ -0,0 +1,16 @@
+
+
+import arithmetic.arithclass; 
+
+public class arith {
+    
+    public static void main(String args[])
+    {
+    arithclass a =new arithclass(); int c = a.add(10,20); 
+    System.out.println("add "+c); 
+    int d = a.sub(20,5);
+     System.out.println("SUB:"+d);
+      int i = a.mul(5,6); 
+      System.out.println("MUL:"+i);
+    }}
+    
---
+++ arithmetic/arithclass.java
@@ -0,0 +1,21 @@
+/**
+ * arithclass
+ */
+
+ package arithmetic;
+public class arithclass {
+
+    public int add(int a,int b)
+{
+return(a+b);
+}
+public int sub(int x, int y)
+{
+return(x-y);
+}
+public int mul(int s,int t)
+{
+return(s*t);
+}
+
+}
